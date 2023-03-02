e8e15076a96b32c7088c637b577a86468a2444a7
hiens <huuhienvip@gmail.com>
2023-03-02 16:06:33 +0700

    First commit: Minimal version

---
+++ lib/flat_breadcrumb.dart
@@ -0,0 +1,115 @@
+library flat_breadcrumb;
+
+import 'dart:math' as math;
+import 'package:flutter/material.dart';
+
+class FlatBreadcrumb extends StatelessWidget {
+  FlatBreadcrumb({super.key});
+  final List<Widget> _sampleItems = [
+    BreadcrumbItem(isFirst: true),
+    BreadcrumbItem(),
+    BreadcrumbItem(isLast: true),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 50,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: _sampleItems.length,
+        itemBuilder: (context, index) => Transform.translate(
+          offset: Offset(-20.0 * index, 0),
+          child: SizedBox(
+            width: 100,
+            height: 50,
+            child: _sampleItems[index],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BreadcrumbItem extends StatelessWidget {
+  const BreadcrumbItem({this.isFirst = false, this.isLast = false, super.key});
+
+  final bool isFirst;
+  final bool isLast;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+        painter: BreadcrumbItemPainter(
+          strokeColor: Colors.black,
+          backgroundColor: Colors.grey,
+          strokeCap: StrokeCap.round,
+          strokeWidth: 1,
+          isFirst: isFirst,
+          isLast: isLast,
+        ),
+        child: SizedBox.expand());
+  }
+}
+
+class BreadcrumbItemPainter extends CustomPainter {
+  const BreadcrumbItemPainter({
+    required this.strokeColor,
+    required this.backgroundColor,
+    required this.strokeWidth,
+    required this.strokeCap,
+    required this.isFirst,
+    required this.isLast,
+  });
+
+  final Color strokeColor;
+  final Color backgroundColor;
+  final double strokeWidth;
+  final StrokeCap strokeCap;
+  final bool isFirst;
+  final bool isLast;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    var paint = Paint()
+      ..color = strokeColor
+      ..strokeWidth = strokeWidth
+      ..strokeCap = strokeCap;
+    final x = size.width;
+    final y = size.height;
+
+    final Path path = Path();
+    path
+      ..moveTo(0, 0)
+      ..lineTo(x - y / 2, 0);
+
+    if (isLast) {
+      path.addArc(
+          Rect.fromCenter(
+              center: Offset(x - y / 2, y / 2), height: y, width: y),
+          3 * math.pi / 2,
+          math.pi);
+    } else {
+      path
+        ..lineTo(x, y / 2)
+        ..lineTo(x - y / 2, y);
+    }
+
+    path.lineTo(0, y);
+
+    if (isFirst) {
+      path.lineTo(0, 0);
+    } else {
+      path
+        ..lineTo(y / 2, y / 2)
+        ..lineTo(0, 0);
+    }
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
